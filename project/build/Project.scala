f41b22ac5033ba0a12d222830496576bc8af2914
Jon-Anders Teigen <mail@jteigen.com>
2011-02-26 21:23:33 +0100

    push it real good

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -3,4 +3,9 @@ import sbt._
 class Project(info:ProjectInfo) extends DefaultProject(info) {
   val constrettoRepo = "constretto-repo" at "http://repo.constretto.org/content/repositories/snapshots"
   val contrettoCore = "org.constretto" % "constretto-core" % "2.0-SNAPSHOT"
+
+  override def managedStyle = ManagedStyle.Maven
+  val publishTo = constrettoRepo
+
+  Credentials(Path.userHome / ".ivy2" / "constretto.properties", log)
 }
