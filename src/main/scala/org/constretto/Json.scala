090d5233abeab3fd458f80211f7f17249c19cd00
Jon-Anders Teigen <mail@jteigen.com>
2011-09-24 12:16:25 +0200

    get:Option for JObject

--- src/main/scala/org/constretto/Json.scala
+++ src/main/scala/org/constretto/Json.scala
@@ -13,6 +13,7 @@ case class JArray(data: List[Json]) extends Json
 
 case class JObject(data: Map[String, Json]) extends Json {
   def apply[A](name:String)(implicit converter:Converter[A]) = converter.convert(data(name))
+  def get[A](name:String)(implicit converter:Converter[A]) = data.get(name).map(converter.convert)
 }
 
 case class JPrimitive(value: String) extends Json
