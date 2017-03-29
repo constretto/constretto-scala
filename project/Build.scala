7a7d8ada4039e1ce7e97b78ab1baac3f9c26f9a0
Kaare Nilsen <kaare.nilsen@gmail.com>
2017-03-29 16:14:45 +0200

    publish 2.12

--- project/Build.scala
+++ project/Build.scala
@@ -3,7 +3,7 @@ import sbt.Keys._
 import scala.Some
 
 object BuildSettings {
-  val version = "1.2-SNAPSHOT"
+  val version = "1.2"
 }
 
 object Settings {
@@ -19,47 +19,19 @@ object Settings {
   lazy val buildSettings = Defaults.defaultSettings ++ Seq(
     organization := "org.constretto",
     version := BuildSettings.version,
-    crossScalaVersions := Seq("2.12.0", "2.11.7", "2.10.5"),
+    crossScalaVersions := Seq("2.12.1", "2.12.0", "2.11.7", "2.10.5"),
     scalaVersion := crossScalaVersions.value.head,
     credentialsSetting,
     publishMavenStyle := true,
     publishArtifact in Test := false,
     pomIncludeRepository := { x => false },
-    shellPrompt := ShellPrompt.buildShellPrompt,
-    publishTo <<= (version) {
-      version: String =>
-        if (version.trim.endsWith("SNAPSHOT"))
-          Some("Sonatype Nexus Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots")
-        else
-          Some("Sonatype Nexus Repository Manager" at "https://oss.sonatype.org/service/local/staging/deploy/maven2")
+    publishTo := {
+      if (isSnapshot.value)
+        Some("Sonatype Nexus Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots")
+      else
+        Some("Sonatype Nexus Repository Manager" at "https://oss.sonatype.org/service/local/staging/deploy/maven2")
     }
   )
-}
-
-object ShellPrompt {
-
-  object devnull extends ProcessLogger {
-    def info(s: => String) {}
-
-    def error(s: => String) {}
-
-    def buffer[T](f: => T): T = f
-  }
-
-  val current = """\*\s+([\w-]+)""".r
-
-  def gitBranches = ("git branch --no-color" lines_! devnull mkString)
-
-  val buildShellPrompt = {
-    (state: State) => {
-      val currBranch =
-        current findFirstMatchIn gitBranches map (_ group (1)) getOrElse "-"
-      val currProject = Project.extract(state).currentProject.id
-      "%s:%s:%s> ".format(
-        currProject, currBranch, BuildSettings.version
-      )
-    }
-  }
 }
 
 object Dependencies {
@@ -87,40 +59,38 @@ object ConstrettoBuild extends Build {
     base = file("."),
     settings = buildSettings ++ Seq(
       description := "Constretto Scala API",
-      pomExtra <<= (pomExtra, name, description) {
-        (extra, name, desc) => extra ++ Seq(
-          <url>http://constretto.org</url>,
-          <licenses>
-            <license>
-              <name>Apache</name>
-              <url>http://www.apache.org/licenses/LICENSE-2.0.txt</url>
-              <distribution>repo</distribution>
-            </license>
-          </licenses>,
-          <scm>
-            <connection>scm:git:git@github.com:constretto/constretto-scala.git</connection>
-            <developerConnection>scm:git:git@github.com:constretto/constretto-scala.git</developerConnection>
-            <url>http://github.com/constretto/constretto-scala</url>
-          </scm>,
-          <issueManagement>
-            <system>github</system>
-            <url>https://github.com/constretto/constretto-scala/issues</url>
-          </issueManagement>,
-          <developers>
-            <developer>
-              <id>kaarenilsen</id>
-              <name>Kaare Nilsen</name>
-              <email>kaare.nilsen@gmail.com</email>
-              <organization>Arktekk AS</organization>
-              <organizationUrl>http://arktekk.no</organizationUrl>
-              <roles>
-                <role>Scala Developer</role>
-              </roles>
-              <timezone>+1</timezone>
-            </developer>
-          </developers>
-        )
-      },
+      pomExtra := Seq(
+        <url>http://constretto.org</url>,
+        <licenses>
+          <license>
+            <name>Apache</name>
+            <url>http://www.apache.org/licenses/LICENSE-2.0.txt</url>
+            <distribution>repo</distribution>
+          </license>
+        </licenses>,
+        <scm>
+          <connection>scm:git:git@github.com:constretto/constretto-scala.git</connection>
+          <developerConnection>scm:git:git@github.com:constretto/constretto-scala.git</developerConnection>
+          <url>http://github.com/constretto/constretto-scala</url>
+        </scm>,
+        <issueManagement>
+          <system>github</system>
+          <url>https://github.com/constretto/constretto-scala/issues</url>
+        </issueManagement>,
+        <developers>
+          <developer>
+            <id>kaarenilsen</id>
+            <name>Kaare Nilsen</name>
+            <email>kaare.nilsen@gmail.com</email>
+            <organization>Arktekk AS</organization>
+            <organizationUrl>http://arktekk.no</organizationUrl>
+            <roles>
+              <role>Scala Developer</role>
+            </roles>
+            <timezone>+1</timezone>
+          </developer>
+        </developers>
+      ),
       libraryDependencies += Dependencies.scalatest,
       libraryDependencies ++= Dependencies.deps
    )
